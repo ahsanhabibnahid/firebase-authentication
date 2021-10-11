a94eb9f366e32c7b522af969063d625c991248e3
Ahsan Habib Nahid <ahsanhabib3239@gmail.com>
2021-10-11 12:53:16 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import firebaseConfig from './firebase.config';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from 'react';
+
+const app = initializeApp(firebaseConfig)
 
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    userName: '',
+    email: '',
+    photo: ''
+  })
+  const provider = new GoogleAuthProvider();
+
+  const handleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const { displayName, email, photoURL } = result.user
+        const signedInUser = {
+          isSignedIn: true,
+          userName: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signedInUser)
+
+      })
+      .catch(error => {
+        const errorMessage = error.message;
+        console.log(errorMessage)
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleSignIn}>Sign In</button>
+      {
+        user.isSignedIn && <div>
+          <p>Welcome {user.userName}</p>
+          <p>Your Email {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBLpm8P8ugGok_0iOhGYMNMAhPDsVU80CU",
+  authDomain: "ema-john-941d8.firebaseapp.com",
+  projectId: "ema-john-941d8",
+  storageBucket: "ema-john-941d8.appspot.com",
+  messagingSenderId: "71893910975",
+  appId: "1:71893910975:web:6b6759db9547a74ab98d0a"
+};
+
+export default firebaseConfig
